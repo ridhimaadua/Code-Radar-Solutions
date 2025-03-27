f36c9fe400cdcec765c4ae0a9b06a0b4f61548a3
Ridhima <ridhimadua12@gmail.com>
2025-03-27 18:56:04 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
-int main (){
-    void reverse (int arr[], int start , int end ){
-        while (start<end){
-            int temp = arr [start];
-            arr[start]= arr[end];
-            start++;
-            end--;
 
-        }
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
     }
-    void rotatearray (int arr[],int n , int k){
-        k=k%n;
-        reverse(arr, 0, n-1);
-        reverse(arr,0,k-1);
-        reverse(arr,k,n-1);
+}
 
-    }
-    int main (){
-        int n , k ;
-        scanf("%d",&n);
-        int arr [n];
-         for (int i =0;i<n ; i++){
-            scanf ("%d",&k);
-            rotatearray(arr,n,k);
-            for (int i =0; i<n ; i++){
-                printf ("%d",arr[i]);
+void rotatearray(int arr[], int n, int k) {
+    k = k % n; // Handle cases where k > n
+    reverse(arr, 0, n - 1);
+    reverse(arr, 0, k - 1);
+    reverse(arr, k, n - 1);
+}
 
-            }
-         }
+int main() {
+    int n, k;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
+    printf("Enter the rotation count: ");
+    scanf("%d", &k);
+    rotatearray(arr, n, k);
+    printf("Rotated array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
 }
